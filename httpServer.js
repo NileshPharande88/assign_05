7dfb4d8b5866bb470dd8185ee236019d5120bd2c
nileshpharande88 <nilesh.pharande@mindstix.com>
2015-03-10 20:26:25 +0530

    change send responce function name to see effect of the series and parallel use of the async module.

--- httpServer.js
+++ httpServer.js
@@ -3,42 +3,212 @@ try {
     var fs = require('fs');
     var qs = require('querystring');
     var jsonReader = require("json-reader");
+    var async = require("async");
 
     //Checks the required modules are available or not.
     if (http === undefined) throw new Error( " Can't access http module" );
     if (fs === undefined) throw new Error( " Can't access fs module" );
     if (qs === undefined) throw new Error( " Can't access qs module" );
     if (jsonReader === undefined) throw new Error( " Can't access json-reader module" );
+    if ((async === undefined)) throw new Error( " Can't access async module" );
 
-
-    var getStudentId = function (res, qParam, object, callback) {
+    //Accepts the JSON object and query string parameter. Returns the error object or the studentId.
+    var getStudentId = function (qParam, object, callback) {
         var students = object.students;
-        if(students === undefined) {
-            console.log(" jsonObject not contain students key.");
-            res.end(" jsonObject not contain students key.");
-        } else if (students.length == 0) {  //checks for the presence of students inan array of the students tag.
-            console.log(" jsonObject not contain students in array.");
-            res.end(" jsonObject not contain students in array.");
+        if (students === undefined) {  //If array of students not found then return with error message.
+            return callback( new Error(" jsonObject not contain students key."), null );
+        } else if (students.length == 0) {  //Checks for the presence of students in an array of the students tag.
+            return callback( new Error(" jsonObject not contain students in array."), null );
         } else {
-
-            students.forEach(function (value) {
-                if( value.email === qParam.email ) {
-                    console.log("student is not in record", qParam.email);
-                    res.end("student is not in record" + qParam.email);
-                } else {
-                    console.log("student id: ", value.id);
-                    res.end("student id: " + value.id);
+            var studentId = -1;
+            for (x in students) {  //Checks in student records for the presence of the email address coming from the qParam. 
+                if( students[x].email === qParam.email ) {
+                    studentId = students[x].id;
                 }
-            });
-
+            }
+            if (studentId === -1) {  //Retur nwith the error if the student id is not found.
+                return callback( new Error(" Record for the email address is not found."), null );
+            } else {  //Returns the studentId.
+                return callback(null, studentId);
+            }
         }
     }
+
+    //Returns the array of names of enrolledSubjects.
+    var getEnrolledSubjectNames = function (subjectArray, studentId, callback) {
+        var enrolledSubjects = [];
+        subjectArray.forEach( function (subject) {  //Saparately access every subject.
+            if (subject.enrolledStudents !== undefined ) {  //Get inside only if subject.enrolledStudents is present.
+                var enrolledStudents = subject.enrolledStudents;
+                for (x in enrolledStudents) {  //Saparately access every enrolledStudent.
+                    if (enrolledStudents[x].id === studentId) { //If studentId matches then added the subject name to the array.
+                        enrolledSubjects.push(subject.subjectName);
+                        break;
+                    }
+                }//for (x in enrolledStudents)
+            }
+        });// subjectArray.forEach()
+        if(enrolledSubjects.length === 0) {
+            return callback(new Error( " Can't found the enrolled subjects for this student." ), null);
+        } else {
+            return callback(null, enrolledSubjects);
+        }
+    }//getEnrolledSubjectNames()
+
+    //Accepts studentId and send response according to it using async.parallel.
+    var sendResponse = function (res, studentId, cb) {
+        var startTime = (new Date()).getTime();
+        async.parallel([
+            function (callback) {  //Reading sub_1.json
+                jsonReader.jsonObject("./sourceFiles/sub_1.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_1.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_2.json
+                jsonReader.jsonObject("./sourceFiles/sub_2.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_2.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_3.json
+                jsonReader.jsonObject("./sourceFiles/sub_3.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_3.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_4.json
+                jsonReader.jsonObject("./sourceFiles/sub_4.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_4.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_5.json
+                jsonReader.jsonObject("./sourceFiles/sub_5.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_5.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            }
+        ],
+        // optional callback 
+        function(err, results){
+            var diff = (new Date()).getTime() -  startTime;
+             console.log("Time required to read subject files in parallel: ", diff);
+            if (err) {  //Returns an error if error occured in reading any of the subject files in json objects.
+                return cb(err, null);
+            } else {  //Process further on the read json objects.
+                getEnrolledSubjectNames(results, studentId, function (err, response) {
+                    if (err) {
+                        return cb(err, response);
+                    } else {
+                        return cb(err, response);
+                    }
+                });//getEnrolledSubjectNames()
+            }
+        });//async.parallel()
+
+    }//sendResponse(parallel).
+
+
+    //Accepts studentId and send response according to it using async.parallel.
+    var sendResponse1 = function (res, studentId, cb) {
+        var startTime = (new Date()).getTime();
+        async.series([
+            function (callback) {  //Reading sub_1.json
+                jsonReader.jsonObject("./sourceFiles/sub_1.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_1.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_2.json
+                jsonReader.jsonObject("./sourceFiles/sub_2.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_2.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_3.json
+                jsonReader.jsonObject("./sourceFiles/sub_3.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_3.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_4.json
+                jsonReader.jsonObject("./sourceFiles/sub_4.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_4.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            },
+            function (callback) {  //Reading sub_5.json
+                jsonReader.jsonObject("./sourceFiles/sub_5.json", function ( err, object ) {
+                    if(err) {
+                        console.log("Failed to read sub_5.json");
+                        callback(err, null);
+                    } else {
+                        callback(err, object);
+                    }
+                });//jsonReader.
+            }
+        ],
+        // optional callback 
+        function(err, results){
+            var diff = (new Date()).getTime() -  startTime;
+             console.log("Time required to read subject files in series: ", diff);
+            if (err) {  //Returns an error if error occured in reading any of the subject files in json objects.
+                return cb(err, null);
+            } else {  //Process further on the read json objects.
+                getEnrolledSubjectNames(results, studentId, function (err, response) {
+                    if (err) {
+                        return cb(err, response);
+                    } else {
+                        return cb(err, response);
+                    }
+                });//getEnrolledSubjectNames()
+            }
+        });//async.parallel()
+
+    }//sendResponse(series).
 
 
 
     var server = http.createServer ( function (req, res) {
-        var folderName = req.url.split('/')[1]; //devided url on base of "/".
-        folderName = folderName.toLowerCase(); //Converte folder name to lower case.
+        var folderName = req.url.split('/')[1];  //devided url on base of "/".
+        folderName = folderName.toLowerCase();  //Converte folder name to lower case.
         if ( folderName === "favicon.ico" ) {
             res.end();   //Avoid un necessory execution of code.
         } else {
@@ -49,24 +219,28 @@ try {
             } else {
                 //Read source.json file using "json-reader" module.
                 jsonReader.jsonObject("./sourceFiles/students.json", function ( err, object ) {
-                    if(err) {
+                    if(err) {  //Error respose if the students.json file is not read.
                         console.log(err);
-                        res.end(err);
-                    } else {
-                        getStudentId(res, qParam, object, function(err, response){
-                            if(err){
-                                ;
+                        res.end("ERROR: Unable to read students.json file.");
+                    } else {  //students.json file is not read correctly.
+                        getStudentId(qParam, object, function(err, studentId){
+                            if(err){  //Sent message in response that no record found for the student.
+                                console.log(err);
+                                res.end("ID not found in record.");
                             } else {
-                                ;
+                                sendResponse(res, studentId, function (err, response) {
+                                    if(err) {
+                                        res.end("Failed to send an response.");
+                                    } else {
+                                        res.writeHead(200, {'Content-Type': 'text/plain' });
+                                        res.end( JSON.stringify( { Books: response } ) );
+                                    }
+                                });//sendResponse().
                             }
-                        });
-                        ;
-                        ;
-                        console.log("student id test");
-                        res.end("student id test");
+                        });//getStudentId().
                     }
-                });
-            }
+                });//jsonReader.
+            }//qParam.email.
         }
     });//Work of createServer is completed.
 
